dd3642268718d02fe5f87ba239c3153aec8c3c98
Moustapha <kammoun.moustapha@gmail.com>
2022-06-07 11:50:26 +0200

    Ajout dossier request
    Ajout du fichier de connexion à la base de données MySQL via php

---
+++ request/01-getTitleMovies.php
@@ -0,0 +1,10 @@
+<?php
+
+// Réaliser une requête qui permet de récupérer tous les titres des films
+
+require_once('../connect.php');
+
+$title = $db->prepare('SELECT title FROM `infos_movies`');
+$title->execute();
+
+$resp = $title->fetch(PDO::FETCH_ASSOC);
